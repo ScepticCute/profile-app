7a865434ce67c45c495528e580c2ee792ceafdcc
Artem Shulgin <artemiyshulginhsl@gmail.com>
2023-01-21 19:30:23 +0700

    Revert "skills array encapsulated in environment variable"
    This reverts commit de5e8538604a0cbea9a0522bb4876d49f4d55095.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,31 @@
-import { useEffect, useState } from "react";
-import { Route, Routes } from "react-router-dom/dist";
-import styles from "./App.module.scss";
-import { Footer } from "./components/Footer/Footer";
-import { Header } from "./components/Header/Header";
-import { Home } from "./pages/Home/Home";
+import { useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom/dist';
+import styles from './App.module.scss';
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
+import { Home } from './pages/Home/Home';
+
 
 function App() {
   const [headerIsActive, setHeaderActive] = useState(false);
   const [burgerIsOpen, setBurgerOpen] = useState(false);
-
-  // Script to mount header in 3.5sec
   setTimeout(() => {
     setHeaderActive(true);
   }, 3500);
-
-  // Script to mount header if the screen moves down
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 100) {
         setHeaderActive(true);
       }
     };
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
-
   return (
     <div className={styles.wrapper}>
-      {headerIsActive ? (
-        <Header setBurgerOpen={setBurgerOpen} burgerIsOpen={burgerIsOpen} />
-      ) : (
-        ""
-      )}
+      {headerIsActive ? <Header setBurgerOpen={setBurgerOpen} burgerIsOpen={burgerIsOpen} /> : ''}
       <Routes>
         <Route path="/" element={<Home arrowIsActive={headerIsActive} />} />
       </Routes>
